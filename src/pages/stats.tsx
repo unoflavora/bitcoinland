7c3a3578735d7bf45906003febffc951babbd07b
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-10-01 05:32:35 +0700

    add section

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,41 +1,76 @@
 import MotionDiv from "./components/motionDiv";
 import ColorizedText from "./components/colorizedText";
+import {motion} from 'framer-motion';
+import Image from "next/image";
 
 export default function Stats()
 {
   const stats = [{
-    title: "+500M Downloads",
+    title: "What is Bitcoin",
     from : "from-red-500",
     to: "to-yellow-400",
-    desc: "We are at the beginning of our mission to deliver fun and over 300 million players have already joined us"
+    
+    desc: "Out of the more than 19,000 cryptocurrencies in use today, Bitcoin (BTC) is one of the more well-known variants. It is a digital currency designed to function as money and a payment method independently of any other person, organisation, or institution, thereby dismissing the requirement for a third party to be involved in ﬁnancial transactions. "
   },
   {
-    title: "15 games live",
+    title: "When Was Bitcoin Founded",
     from : "from-blue-400",
     to: "to-grey-300",
-    desc: "Creating amazing games is our DNA! Come and discover our latest games and future releases."
+    desc: "Since its public release in 2009, its value has risen abundantly. As of July 10th, 2023, the value of a single Bitcoin currency has climbed from its original value of $0 to $30,090.90"
+  },
+  {
+    title: "How is Bitcoin Ussd",
+    from : "from-blue-400",
+    to: "to-grey-300",
+    desc: "The Bitcoin transaction operates on a public decentralised ledger which makes it difﬁcult for those to falsify or reverse any transactions. As a form of digital currency, it operates by an individual owning a Bitcoin which can be distributed in smaller portions as payment for products. One Bitcoin can be divided up to eight decimal places, the smallest unit known as a satoshi. If this is accepted, Bitcoin can be divisible into more decimal places."
   }
 ]
-  return <div className="flex flex-col gap-10 px-5 py-5 md:px-10 lg:px-36">
+  return <div className="flex flex-col gap-20 px-5 py-5 md:px-10 lg:px-36">
+
+  <div className="flex flex-col gap-20 lg:flex-row">
+    <motion.div
+       viewport={{once : true}}
+       initial={{opacity: 0}}
+       whileInView={{opacity: 1}}
+       className="flex justify-center">
+      <Image src="/company/logo.png" width={200} height={100} alt="company logo"/>
+    </motion.div>
+    <MotionDiv>
+          <h1 className='text-5xl lg:text-7xl text-center font-bold 
+          text-transparent  bg-clip-text bg-gradient-to-r from-black to-gold 
+          '> Bitcoinland</h1>
+          <p className="text-black lg:text-3xl lg:px-36 ">
+          BitcoinLand is the brainchild of Rimaunangis $RXT. BitcoinLand.com was registered in 2011, with the vision of eventually creating a Bitcoin theme park or smart city, inspired by LegoLand.
+          The idea for constructing the Bitcoin Iconic building and MasterplanCity came to live in 2023.
+          </p>
+    </MotionDiv>
+  </div>
+
+
    <MotionDiv>
-        <h1 className='text-5xl lg:text-7xl 2xl:text-9xl text-black text-center font-bold pb-10'> Discover our mad dream of making the world play!</h1>
+        <h1 className='text-4xl lg:text-7xl text-center font-bold 
+        text-transparent  bg-clip-text bg-gradient-to-r from-black to-gold py-2
+        '>Understanding Bitcoin</h1>
+        <p className="text-black lg:text-3xl lg:px-36">
+        BitcoinLand is the brainchild of Rimaunangis $RXT. BitcoinLand.com was registered in 2011, with the vision of eventually creating a Bitcoin theme park or smart city, inspired by LegoLand.
+         The idea for constructing the Bitcoin Iconic building and MasterplanCity came to live in 2023.
+        </p>
    </MotionDiv>
 
-    <div className="flex flex-col md:flex-row md:grid md:grid-cols-2 gap-5 2xl:px-56">
-        {stats.map((stat, i) => <MotionDiv key={'stat' + i}>
-            <div className={`py-10 px-5 text-3xl lg:text-5xl 2xl:text-7xl lg:px-10 lg:p-24 lg:py-36 h-full flex flex-col gap-4 bg-gradient-to-b from-red-100 to-yellow-100 rounded-2xl`}>
-                <ColorizedText text={stat.title} from={stat.from} to={stat.to}/>
+    <div className="flex flex-col gap-5 2xl:px-56">
+    <h1 className='text-4xl lg:text-7xl text-center font-bold 
+        text-transparent  bg-clip-text bg-gradient-to-r from-black to-gold pb-5
+        '>Innovating Bitcoin</h1>
+
+        {stats.map((stat, i) => 
+        <MotionDiv key={'stat' + i}>
+            <div className={`py-10 px-5 text-3xl lg:text-5xl 2xl:text-7xl lg:px-10 lg:p-24 lg:py-36 h-full flex flex-col gap-4 bg-gradient-to-b from-[#F5F5F5] to-[#F5F5F5] rounded-2xl`}>
+                <ColorizedText text={stat.title} from={'from-black'} to={'to-black'}/>
                 <p className="text-black text-base lg:text-3xl">{stat.desc}</p>
             </div>
         </MotionDiv>)}
 
 
-    </div>
-        
-    <div className="w-full 2xl:px-56">
-      <div className="bg-gray-300 w-full h-36 lg:h-96 rounded-xl flex justify-center items-center ">
-          Image will appear here
-      </div>
     </div>
   
   </div>
