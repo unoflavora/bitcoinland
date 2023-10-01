123ce5d02ece6c8dfbd09d34284fc178172f8491
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-10-01 15:36:24 +0700

    contact us

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,34 +5,27 @@ import {motion} from 'framer-motion'
 
 export default function Index()
 {
-    return <div className="w-full h-full flex justify-center  lg:gap-20 lg:py-44 px-5 md:px-24">
+    return <div className="w-full h-full flex justify-center pt-20  lg:gap-5 lg:py-44 px-5 md:px-24">
         <div className="max-w-7xl flex flex-col lg:grid lg:grid-cols-2 lg:gap-20 lg:justify-center lg:items-center">
-        <div className="w-full  flex flex-col gap-20 md:gap-5 py-24 ">
-            <motion.div
+        <div className="w-full  flex flex-col gap-20 md:gap-5 py-10 ">
+            <motion.div 
                 whileInView={{y: 0}}
                 initial={{y: "-50%"}}
                 viewport={{once: true}}
-                 className="w-full h-56 lg:h-64">
-                <TemplateImage/>
-
-            </motion.div>
-
-            <motion.div 
-                whileInView={{y: 0}}
-                initial={{y: "50%"}}
-                viewport={{once: true}}
                 className="text-4xl lg:text-5xl flex flex-col gap-2">
-                <h1 className={"font-extrabold text-transparent mb-1.5 bg-clip-text bg-gradient-to-r py-2 from-grey to-gold "}>Who You gonna call?</h1>
+                <h1 className={"font-extrabold text-transparent mb-1.5 bg-clip-text bg-gradient-to-r py-2 from-grey to-gold "}>Contact Us</h1>
                     <p className="text-base lg:text-xl text-black">
                     Looking to make a media inquiry? Any feedback or sugestions? Fill out the form and we will get back to you!
                 </p>
             </motion.div>
         </div>
-        <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} viewport={{once: true}} className="flex justify-center ">
-            <div className="w-full">
-                <Form/>
 
-            </div>
+        <motion.div
+          whileInView={{y: 0}}
+          initial={{y: "50%"}}
+          viewport={{once: true}} 
+        className="flex justify-center pb-10">
+           <Form/>
 
         </motion.div>
         </div>
