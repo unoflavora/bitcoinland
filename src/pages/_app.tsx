1a323951f00a1bdda421f4697f8df93fe4fe86a3
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-10-01 18:49:19 +0700

    update image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,8 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content="Bitcoinland"/>
-      <meta property="og:type" content="article"/>
-      <meta property="og:image" content="https://i.ibb.co/tJCPH0j/ss.png"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:image" content="https://i.ibb.co/c8Jm0LN/ss.png"/>
       <meta property="og:url" content="bitcoinland.vercel.app"/>
       <meta name="twitter:card" content="summary_large_image"/>
 
