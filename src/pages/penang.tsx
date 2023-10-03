f62c65bf444be7da93bc318fd1acfdc9e202e661
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-10-03 16:49:08 +0700

    adjust timing

--- src/pages/penang.tsx
+++ src/pages/penang.tsx
@@ -31,7 +31,7 @@ export default function Penang()
                     </div>
                 </MotionDiv>
                 <motion.div
-                    transition={{ease: easeInOut, duration: .5}}
+                    transition={{ease: "linear", duration: 1}}
                     initial={{scale: 0}}
                     whileInView={{scale: 1}}
                     viewport={{once : true}}
@@ -53,7 +53,7 @@ export default function Penang()
                 </MotionDiv>
 
                 <motion.div
-                    transition={{ease: easeInOut, duration: 1}}
+                    transition={{ease: "easeInOut", duration: 1}}
                     initial={{scale: 0}}
                     whileInView={{scale: 1}}
                     viewport={{once : true}}
