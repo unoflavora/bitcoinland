f17d367d2de0ee32f1c48b50b916b599bded4fd8
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-10-07 15:14:43 +0700

    add socials

--- src/pages/_components/footer.tsx
+++ src/pages/_components/footer.tsx
@@ -6,17 +6,21 @@ export default function Footer()
     return <div  className="w-full min-h-full py-8  px-4 lg:px-36 gap-7 lg:gap-14 lg:py-10 flex flex-col justify-center lg:justify-between items-center bg-grey">
         
         <div className="flex flex-col justify-center items-center gap-7 lg:flex-row lg:justify-between w-full">
-            <Image className="h-20 lg:h-24 w-fit aspect-square" src="/icon.png" width={500} height={500} alt="company-logo"/>
+            <Link href="https://www.rimaunangis.world/">
+                <Image className="h-20 lg:h-24 w-fit aspect-square" src="/icon.png" width={500} height={500} alt="company-logo"/>
+            </Link>
             
 
             <div className="flex flex-col justify-center items-center lg:justify-end lg:items-end gap-7">
-                <div className="flex  lg:w-full lg:justify-end  gap-8">
+                <div className="flex flex-wrap justify-center lg:w-full lg:justify-end  gap-8">
                     {[
                         {title: 'Instagram', url : 'https://www.instagram.com/rxt_token'}, 
                         {title: 'Twitter', url : 'https://twitter.com/RimaunangisRXT'}, 
                         {title: 'Facebook', url : 'https://www.facebook.com/people/Rimaunangis-Capital-LTD/100084889096948/'}, 
                         {title: 'Youtube', url : 'https://www.youtube.com/@rimaunangisdigitalllc3198'}, 
-                        {title: 'Tiktok', url : 'https://www.tiktok.com/@rxt_token?_t=8eiiaawuxva&_r=1'}].map((logo, i) => 
+                        {title: 'Tiktok', url : 'https://www.tiktok.com/@rxt_token?_t=8eiiaawuxva&_r=1'},
+                        {title: 'Discord', url: 'https://discord.gg/yv6CDTuX'},
+                        {title: 'Telegram', url: 'https://t.me/rimaunangis_official'}].map((logo, i) => 
                             <Link className="hover:scale-110" key={`social logo ${logo.title}`} href={logo.url}>
                                 <Image src={`/icons/${logo.title}.svg`} width={40} height={30} alt={`social logo ${logo.title}`}/>
                             </Link>
